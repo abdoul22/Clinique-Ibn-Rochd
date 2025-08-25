6d4103eb26dd39765b5320dfdf9633258b122ee7
Abdou <abdoullaha442@gmail.com>
2025-08-25 02:45:08 +0000

    - fix personnal bugs   V.1.5

--- resources/views/personnels/index.blade.php
+++ resources/views/personnels/index.blade.php
@@ -165,16 +165,3 @@
     </div>
 </div>
 @endsection
-
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
-    </div>
-
-    <!-- Pagination -->
-    <div class="mt-4">
-        {{ $personnels->links() }}
-    </div>
-</div>
-@endsection
